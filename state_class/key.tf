d8a6149f90a740152790b21fe2035a79cd12b8fd
EC2 Default User <ec2-user@ip-172-31-5-215.us-west-1.compute.internal>
2020-04-30 10:17:38 +0000

    state project complate &added task3

--- state_class/key.tf
+++ state_class/key.tf
@@ -4,3 +4,7 @@ resource "aws_key_pair" "us-east-1-key" {
   public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
 
+resource "aws_key_pair" "us-east-1-key2" {
+  key_name   = "state_class_key2"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
