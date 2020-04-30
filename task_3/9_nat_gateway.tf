76ba91dae4b604941b0167c372608c45a1acedfa
EC2 Default User <ec2-user@ip-172-31-5-215.us-west-1.compute.internal>
2020-04-30 11:56:34 +0000

    update

---
+++ task_3/9_nat_gateway.tf
@@ -0,0 +1,13 @@
+resource "aws_eip" "allocation_ip" {
+  vpc      = true
+
+}
+
+resource "aws_nat_gateway" "task3_ng" {
+  allocation_id = "${aws_eip.allocation_ip.id}"
+  subnet_id     = "${aws_subnet.task3_public1.id}"
+
+ tags = {
+    Name = "task3_NG"
+  }
+}
