3caa4072f57a54596facd15c901879decf6edfba
EC2 Default User <ec2-user@ip-172-31-5-215.us-west-1.compute.internal>
2020-05-05 22:30:42 +0000

    update

--- app/5_asg_module.tf
+++ app/5_asg_module.tf
@@ -1,4 +1,11 @@
 module "app1" { 
-source = "./module" 
-
+source = "./module"
+region =  "us-east-1"
+owners = "137112412989"
 } 
+
+module "wordpress" { 
+source = "./module" 
+region =  "us-east-2"
+owners = "137112412989"
+}
--- app/module/2_image.tf
+++ app/module/2_image.tf
@@ -1,4 +1,10 @@
+# data "aws_ami" "image" {
+#   most_recent = true
+#   owners      = ["137112412989"] # Canonical 
+# }
+
+
 data "aws_ami" "image" {
   most_recent = true
-  owners      = ["137112412989"] # Canonical 
+  owners      = ["${var.owners}"]       # Canonical/Standard
 }
